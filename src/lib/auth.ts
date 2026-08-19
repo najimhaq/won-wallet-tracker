9aa3320290a6e3497aa6dc774f9c15f68adf2690
najimhaq <mdnajimulhaque@gmail.com>
2026-08-19 18:09:44 +0900

    auth updated and express types added

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,28 @@
 import { prismaAdapter } from '@better-auth/prisma-adapter';
 import { betterAuth } from 'better-auth';
 import { prisma } from './db.js';
+import { env } from '../config/env.js';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  baseURL: env.BETTER_AUTH_URL,
+  secret: env.BETTER_AUTH_SECRET,
+  trustedOrigins: [env.CLIENT_URL],
+
   emailAndPassword: {
     enabled: true,
+  },
+  user:{
+    additionalFields:{
+      role:{
+        type:"string",
+        required: false,
+        defaultValue:"USER",
+        input:false,
+      }
+    }
   },
   session: {
     cookieCache: {
